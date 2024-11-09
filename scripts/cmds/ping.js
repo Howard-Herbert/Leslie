be71da18cfaa67abe6b58f0a898ac5809f537519
Aiden-Josiah <jpsiddik93@gmail.com>
2024-11-09 23:07:21 +0600

    Create ping.js

---
+++ scripts/cmds/ping.js
@@ -0,0 +1,15 @@
+module.exports = {
+  config: {
+    name: "ping",
+    version: "1.0",
+    author: "SK-SIDDIK-KHAN",
+    role: 0,
+  },
+  onStart: async function ({ api, event, args }) {
+    const timeStart = Date.now();
+    await api.sendMessage("Checking", event.threadID); 
+    const timeEnd = Date.now();
+    const ping = timeEnd - timeStart; 
+    api.sendMessage(`The current ping is ${ping}ms.`, event.threadID); 
+  }
+};
